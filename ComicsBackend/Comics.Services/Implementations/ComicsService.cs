99660bd5e8f11a204fa12749aa898a0c7e4eceb5
luck-o-o-luck <pks2002@yandex.ru>
2023-02-06 23:58:54 +0300

    fix: fixed DbContext use

--- ComicsBackend/Comics.Services/Implementations/ComicsService.cs
+++ ComicsBackend/Comics.Services/Implementations/ComicsService.cs
@@ -10,40 +10,42 @@ public class ComicsService : IComicsService
 {
     private static readonly HttpClient _client = new();
     private readonly Random _random = new();
-    
+    private readonly ComicsDbContext _context;
+ 
+    public ComicsService()
+    {
+        _context = new ComicsDbContext();
+    }
+
     public async Task<ComicBook> GetComicsRandom()
     {
-        await using var db = new ComicsDbContext();
-        
         var random = _random.Next(1, 2730);
 
-        while (db.ComicBooks.Any(x => x.Id == random))
+        while (_context.ComicBooks.Any(x => x.Id == random))
         {
             random = _random.Next(1, 2730);
         }
-        
+
         var url = $"https://xkcd.com/{random}/info.0.json";
 
         var comics = await _client.GetFromJsonAsync<ComicBook>(url);
 
-        await db.AddAsync(comics);
-        await db.SaveChangesAsync();
+        await _context.AddAsync(comics);
+        await _context.SaveChangesAsync();
 
         return comics;
     }
 
     public async Task DeleteComics(ComicsModel comics)
     {
-        await using var db = new ComicsDbContext();
-
-        var searchedComics = await db.ComicBooks.FirstOrDefaultAsync(x => x.Id == comics.Id);
+        var searchedComics = await _context.ComicBooks.FirstOrDefaultAsync(x => x.Id == comics.Id);
 
         if (searchedComics is null)
         {
             throw new Exception("Comics with this id doesn't exist.");
         }
 
-        db.ComicBooks.Remove(searchedComics);
-        await db.SaveChangesAsync();
+        _context.ComicBooks.Remove(searchedComics);
+        await _context.SaveChangesAsync();
     }
 }
